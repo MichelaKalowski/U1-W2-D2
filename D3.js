485656432b57087b50cfa4ee838728cc22669650
MichelaKalowski <mi.ky90@hotmail.com>
2022-12-06 18:46:02 +0100

    aggiornamento

--- D3.js
+++ D3.js
@@ -115,6 +115,14 @@ else {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let val = 13
+if (val >= 5 && val < 10){
+  console.log("Meno di 10");
+}else if (val < 5){
+  console.log("Meno di 5");-
+}else if ( val >= 10){
+  console.log("Uguale a 10 o maggiore");
+}
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
